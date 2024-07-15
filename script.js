ada99f879fe4a59582afc315b5290f139d13ab56
Ramji Gupta <ramjigupta8310@gmail.com>
2024-07-15 15:12:20 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// mobile navigation menu
 const bar = document.querySelector(".bar");
 const close = document.querySelector("#close");
 
@@ -15,3 +16,17 @@ if(close){
         nav.classList.remove("active");
     })
 }
+
+// active links
+
+// const navLinks=document.querySelectorAll(".nav-links");
+// // console.log(navLinks);
+
+// navLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//         // Remove active class from all links
+//         navLinks.forEach(navLink => navLink.classList.remove('active'));
+//         // Add active class to the clicked link
+//         link.classList.add('active');
+//     });
+// });
